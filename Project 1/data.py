5340c67a02e275828276eaacbd4021daf7ae5261
kushedow <Polemika2020>
2019-12-15 08:57:50 +0300

    initial

---
+++ Project 1/data.py
@@ -0,0 +1,151 @@
+title = "Stepik Travel"
+subtitle = "Для тех, кого отвлекают дома"
+description = "Лучшие направления,где никто не будет вам мешать сидеть на берегу и изучать программирование, дизайн, разработку игр и управление продуктами"
+
+tours = {
+    1: {
+        "title": "Marina Lake Hotel & Spa",
+        "description" : "Отель выглядит уютно. Он был построен из красного соснового дерева и украшен синими камнями.  Высокие округлые окна добавляют общий стиль дома и были добавлены в дом в довольно симметричном образце.",
+        "from":"nsk",
+        "picture": "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "62 000",
+        "stars": "4",
+        "country": "Куба",
+    },
+    2: {
+        "title": "Baroque Hotel",
+        "description" : "Здание отеля имеет форму короткой буквы U. Два расширения связаны стеклянными нависающими панелями. Второй этаж такого же размера, как и первый, который был построен точно над полом под ним. Этот этаж имеет совершенно другой стиль, чем этаж ниже.",
+        "from": "ekb",
+        "picture": "https://images.unsplash.com/photo-1445019980597-93fa8acb246c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "85 000",
+        "stars": "5",
+        "country":"Вьетнам",
+    },
+    3: {
+        "title": "Voyager Resort",
+        "description": "Снаружи отель выглядит красиво и традиционно. Он был построен с белыми камнями и имеет еловые деревянные украшения. Высокие, большие окна добавляют к общему стилю дома и были добавлены в дом в основном симметричным способом.",
+        "from": "nsk",
+        "picture": "https://images.unsplash.com/photo-1569660072562-48a035e65c30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "63 000",
+        "stars": "3",
+        "country":"Пакистан",
+        },
+    4: {
+        "title": "Orbit Hotel",
+        "description": "Каждый домик оборудован средней кухней и одной небольшой ванной комнатой, в нем также есть уютная гостиная, две спальни, скромная столовая и большой подвал.  Небольшие треугольные окна добавляют к общему стилю дома и были добавлены в дом в основном симметричным способом.",
+        "from": "msk",
+        "picture": "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "62 000",
+        "stars": "4",
+        "country":"Индия",
+    },
+    5: {
+        "title": "Atlantis Cabin Hotel",
+        "description": "",
+        "from": "msk",
+        "picture": "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "68 000",
+        "stars": "4",
+        "country":"Доминикана",
+    },
+    6: {
+        "title": "Light Renaissance Hotel",
+        "description": "",
+        "from": "spn",
+        "picture": "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "",
+        "stars": "3",
+        "country": "Пакистан",
+    },
+    7: {
+        "title": "King's Majesty Hotel",
+        "description": "",
+        "from": "ekb",
+        "picture": "https://images.unsplash.com/photo-1468824357306-a439d58ccb1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "72 000",
+        "stars": "5",
+        "country": "Мексика"
+    },
+    8: {
+        "title": "Crown Hotel",
+        "description": "",
+        "from": "kazan",
+        "picture": "https://images.unsplash.com/photo-1549109786-eb80da56e693?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "44 000",
+        "stars": "4",
+        "country": "Тайланд"
+    },
+    9: {
+        "title": "Seascape Resort",
+        "description": "",
+        "from": "nsk",
+        "picture": "https://images.unsplash.com/photo-1570214476695-19bd467e6f7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "39 000",
+        "stars": "3",
+        "country": "Индия"
+    },
+    10: {
+        "title": "Rose Sanctum Hotel",
+        "description": "",
+        "from": "msk",
+        "picture": "https://images.unsplash.com/photo-1560200353-ce0a76b1d438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "52 000",
+        "stars": "4",
+        "country": "Куба"
+    },
+    11: {
+        "title": "Viridian Obelisk Hotel & Spa",
+        "description": "",
+        "from": "spb",
+        "picture": "https://images.unsplash.com/photo-1477120128765-a0528148fed2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "68 000",
+        "stars": "5",
+        "country": "Индия"
+    },
+    12: {
+        "title": "Saffron Tundra Hotel & Spa",
+        "description": "",
+        "from": "kazan",
+        "picture": "https://images.unsplash.com/photo-1440151050977-247552660a3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "72 000",
+        "stars": "4",
+        "country": "Мексика"
+    },
+    13: {
+        "title": "Traveller Resort",
+        "description": "",
+        "from": "ekb",
+        "picture": "https://images.unsplash.com/photo-1553653924-39b70295f8da?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        "price": "49 000",
+        "stars": "3",
+        "country": "Куба"
+    },
+    14: {
+        "title": "History Hotel & Spa",
+        "description": "Крыша высокая, треугольная, многослойная, покрыта пшеничной соломой. Две большие трубы находятся по обе стороны от дома. Многие меньшие окна пропускают много света в комнаты под крышей.Сам дом окружен асфальтированной землей, с местом для еды и отдыха на открытом воздухе и различными горшечными растениями.",
+        "from": "kazan",
+        "picture": "https://images.unsplash.com/photo-1509600110300-21b9d5fedeb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "91 000",
+        "stars": "5",
+        "country": "Вьетнам"
+    },
+    15: {
+        "title": "Riverside Lagoon Hotel & Spa",
+        "description": "Здание имеет форму круга. Дом частично окружен деревянными нависающими панелями с двух сторон. Второй этаж меньше первого, что позволило создать несколько балконов по бокам дома. Этот этаж следует тому же стилю, что и этаж ниже.",
+        "from": "spb",
+        "picture": "https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "price": "82 000",
+        "stars": "4",
+        "country": "Доминикана"
+    },
+    16: {
+         "title": "History Hotel & Spa",
+         "description": "Крыша высокая, треугольная, многослойная, покрыта пшеничной соломой. Две большие трубы находятся по обе стороны от дома. Многие меньшие окна пропускают много света в комнаты под крышей.Сам дом окружен асфальтированной землей, с местом для еды и отдыха на открытом воздухе и различными горшечными растениями.",
+         "from": "spb",
+         "picture": "https://images.unsplash.com/photo-1564056095795-4d63b6463dbf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+         "price": "74 000",
+         "stars": "5",
+         "country": "Вьетнам"
+     }
+
+}
