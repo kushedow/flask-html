ac967b159217004e06e4ff9dd5a2c46403bd5323
kushedow <Polemika2020>
2020-01-23 04:53:32 +0300

    Django project mock data added

---
+++ Django Project 2/data.py
@@ -0,0 +1,48 @@
+""" Вакансии """
+
+jobs = [
+
+    {"title": "Разработчик на Python", "cat": "backend", "company": "staffingsmarter", "salary_from": "100000",
+     "salary_to": "150000", "posted": "2020-03-11", "desc": "Потом добавим"},
+    {"title": "Разработчик в проект на Django", "cat": "backend", "company": "swiftattack", "salary_from": "80000",
+     "salary_to": "90000", "posted": "2020-03-11", "desc": "Потом добавим"},
+    {"title": "Разработчик на Swift в аутсорс компанию", "cat": "backend", "company": "swiftattack",
+     "salary_from": "120000", "salary_to": "150000", "posted": "2020-03-11", "desc": "Потом добавим"},
+    {"title": "Мидл программист на Python", "cat": "backend", "company": "workiro", "salary_from": "80000",
+     "salary_to": "90000", "posted": "2020-03-11", "desc": "Потом добавим"},
+    {"title": "Питонист в стартап", "cat": "backend", "company": "primalassault", "salary_from": "120000",
+     "salary_to": "150000", "posted": "2020-03-11", "desc": "Потом добавим"}
+
+]
+
+""" Компании """
+
+companies = [
+
+    {"title": "workiro"},
+    {"title": "rebelrage"},
+    {"title": "staffingsmarter"},
+    {"title": "evilthreat h"},
+    {"title": "wirey "},
+    {"title": "swiftattack"},
+    {"title": "troller"},
+    {"title": "primalassault"}
+
+]
+
+""" Категории """
+
+categories = [
+
+    {"code": "frontend", "title": "Фронтенд"},
+    {"code": "backend", "title": "Бэкенд"},
+    {"code": "gamedev", "title": "Геймдев"},
+    {"code": "devops", "title": "Девопс"},
+    {"code": "design", "title": "Дизайн"},
+    {"code": "products", "title": "Продукты"},
+    {"code": "management", "title": "Менеджмент"},
+    {"code": "testing", "title": "Тестирование"},
+    {"code": "ios", "title": "Разработка под iOS"},
+    {"code": "android", "title": "Разработка под Android"}
+
+]
