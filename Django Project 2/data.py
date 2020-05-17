4a1fd5de62d2d7e4f9cf696260825000b8c36f7d
kushedow <Polemika2020>
2020-05-17 14:51:40 +0300

    Django project: auth fixes

--- Django Project 2/data.py
+++ Django Project 2/data.py
@@ -44,3 +44,39 @@ specialties = [
     {"code": "testing", "title": "Тестирование"}
 
 ]
+
+""" Статусы в формате Enum """
+
+#
+#
+# class EducationChoices(Enum):
+#     missing = 'Отсутствует'
+#     secondary = 'Среднее'
+#     vocational = 'Средне-специальное'
+#     incomplete_higher = 'Неполное высшее'
+#     higher = 'Высшее'
+#
+#
+# class GradeChoices(Enum):
+#     intern = 'intern'
+#     junior = 'junior'
+#     middle = 'middle'
+#     senior = 'senior'
+#     lead = 'lead'
+#
+#
+# class SpecialtyChoices(Enum):
+#     frontend = 'Фронтенд'
+#     backend = 'Бэкенд'
+#     gamedev = 'Геймдев'
+#     devops = 'Девопс'
+#     design = 'Дизайн'
+#     products = 'Продукты'
+#     management = 'Менеджмент'
+#     testing = 'Тестирование'
+#
+#
+# class WorkStatusChoices(Enum):
+#     not_in_search = 'Не ищу работу'
+#     consideration = 'Рассматриваю предложения'
+#     in_search = 'Ищу работу'
